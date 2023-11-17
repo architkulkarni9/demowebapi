391514825a1848d4b4e71e169040c0f400d15fe6
Iamneo <techadmin@iamneo.ai>
2023-11-17 10:48:15 +0000

    Backup Commit

--- jwt/Controllers/AuthController.cs
+++ jwt/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace jwt.Controllers;
 
@@ -19,10 +20,21 @@ public class AuthController:ControllerBase{
         if(user is null){
             return BadRequest("Invalid Client request");
         }
-        if(user.UserName=="Tom" && user.Password=="Jerry"){
-            var SecretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-            var signingCredentials = new SigningCredentials(secretKey,SecurityAlgorithms.HmacSha256);
-            var TokenOptions=new
+        if(user.UserName == "Tom" && user.Password == "Jerry")
+        {
+            var secretKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+            var signinCredentials=new SigningCredentials(secretKey,SecurityAlgorithms.HmacSha256);
+            var tokenOptions = new JwtSecurityToken(
+                issuer:"http//0.0.0.0:8080",
+                audience:"http//0.0.0.0:8080",
+                claims:new List<Claim>(),
+                expires:DateTime.Now.AddMinutes(10),
+                signinCredentials:signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new AuthenticateResponse{Tokent=tokenString});
         }
+        return Unauthorized();
+       }
     }
-}
+
