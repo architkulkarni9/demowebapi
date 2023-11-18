e591ff9f2f58bcc2f32da32941ea3967451e45c5
Iamneo <techadmin@iamneo.ai>
2023-11-18 10:12:52 +0000

    Backup Commit

---
+++ Demowebapi/Migrations/20231118100108_InitialSetup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demowebapi.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    deptid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    deptname = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.deptid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    empid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    empname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    empsalary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    deptid = table.Column<int>(type: "int", nullable: false),
+                    Departmentdeptid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.empid);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_Departmentdeptid",
+                        column: x => x.Departmentdeptid,
+                        principalTable: "Departments",
+                        principalColumn: "deptid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Departmentdeptid",
+                table: "Employees",
+                column: "Departmentdeptid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
--- Demowebapi/Model/Employee.cs
+++ Demowebapi/Model/Employee.cs
@@ -3,11 +3,13 @@ using System.ComponentModel.DataAnnotations;
 namespace Demowebapi.Model;
 
 public class Employee{
+
+    [Key]
     public int empid{get;set;}
     public string empname{get;set;}
     public decimal empsalary{get;set;}
     public int deptid{get;set;}
-    
+
     public Department Department{get;set;}
     
 }
